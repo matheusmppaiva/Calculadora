b46720811e9e1e556f7ba77e9fd4dbeb14b0ff37
matheusmppaiva <matheus.paiva@gec.inatel.br>
2022-03-18 20:47:43 -0300

    Adicionando o pacote src com as classes main e de operacoes

---
+++ src/main/java/br/inatel/matheus/Operacoes.java
@@ -0,0 +1,21 @@
+package br.inatel.matheus;
+
+public class Operacoes {
+
+        public static double soma(double a, double b){
+            return a + b;
+        }
+
+        public static double subtrai(double a, double b){
+            return a - b;
+        }
+
+        public static double multiplica(double a, double b){
+            return a * b;
+
+        }
+
+        public static double divide(double a, double b) throws ArithmeticException{
+            return a / b;
+        }
+}
---
+++ src/main/java/br/inatel/matheus/main.java
@@ -0,0 +1,11 @@
+package br.inatel.matheus;
+
+public class main {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 3;
+
+        double c = Operacoes.multiplica(a,b);
+        System.out.println(c);
+    }
+}
